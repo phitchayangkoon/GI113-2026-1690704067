bfca0302458641ca6d28c6b286f6a0053f178b23
phitchayangkoon <pirchangkoon@gmail.com>
2026-09-07 17:07:25 +0700

    Update Program.cs

--- Lab03/Program.cs
+++ Lab03/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const int MaxLevel = 10;
+
+            var bossName = "Kirin";   // ต้องประกาศด้วย var ห้ามเขียน string ตรงๆ
+            var rank = 'S';            // ต้องประกาศด้วย var ห้ามเขียน char ตรงๆ
+            int level = 7;
+            int maxHp = 240;
+            int currentHp = 115;       // ค่าตั้งต้นของ Lab นี้คือ HP "หลังโดนโจมตี" จาก Lab 2 แล้ว ไม่ใช่ 175
+            float attackPower = 42.5f;
+            double critMultiplier = 1.75;
+            bool isBoss = true;
+            Console.WriteLine("===== KIRIN SAVE CONVERTER =====");
+
+
+                 
         }
     }
 }
