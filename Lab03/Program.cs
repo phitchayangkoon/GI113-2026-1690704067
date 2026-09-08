0642551af70123c3c984e1e7c0b3b623ea0e2f66
phitchayangkoon <pirchangkoon@gmail.com>
2026-09-08 15:47:07 +0700

    up

--- Lab03/Program.cs
+++ Lab03/Program.cs
@@ -1,4 +1,11 @@
-﻿namespace Lab03
+﻿/*
+ * Student ID :1690704067
+ * Name       :Lap02
+ * Section    :129D
+ * No.        :
+ * Course     : GI113 Computer Programming (GI)
+ */
+namespace Lab03
 {
     internal class Program
     {
---
+++ Lab04/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Student ID :1690704067
+ * Name       :Lap02
+ * Section    :129D
+ * No.        :
+ * Course     : GI113 Computer Programming (GI)
+ */
+
+
+namespace Lab04
+{
+    internal class Program
+        
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+------------------------------+");
+            Console.WriteLine("|      CHARACTER CREATION       |");
+            Console.WriteLine("+------------------------------+");
+            Console.Write("Name your character: ");
+            string charName = Console.ReadLine();
+            Console.Write("Choose a class (1-3): ");
+            bool classOk = int.TryParse(Console.ReadLine(), out int classNum);
+            Console.Write("Starting luck (0.0-10.0): ");
+            bool luckOk = double.TryParse(Console.ReadLine(), out double luck);
+            Console.WriteLine($"{charName} the Class-{classNum} adventurer enters the dungeon. Luck: {luck}");
+        }
+    }
+}
